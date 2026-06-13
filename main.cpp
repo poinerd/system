977919a436100ed0638ebe1520f6806319c394f3
Obadofin Emmanuel <obadofinadedapo123@gmail.com>
2026-06-13 14:45:40 +0100

    system?

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Print text to the terminal, followed by a new line
+    std::cout << "Hello, World! Welcome to C++ systems programming.\n";
+    
+    // Return 0 to tell the Operating System that the program executed successfully
+    return 0;
+}
