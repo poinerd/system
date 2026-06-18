4cf652c732c34da1a78af246f8397c5d260b1445
Obadofin Emmanuel <obadofinadedapo123@gmail.com>
2026-06-18 10:13:50 +0100

    still learning

--- Learn/hello.cpp
+++ Learn/hello.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+
+// you could use an  "#include "iostream"  if iostream is in the same project folder
+
 using namespace std;
 
 int main(){
-
     cout << "Welcome to cpp";
 }
 
 // what is the meaning of the auto type in cpp?
 // what is a namespace in cpp?
 // what is a vector in cpp?
-// what is a contexpr in cpp?
+// what is a contexpr in cpp? a constant expression is tells the copiler to get the eresult of the experession at compile time and not run time 
 // whats teh difference between a referenec and a pointer in cpp and are they different from other stuff in other lang
+
+
+// a rference is like a nickname for a variable
